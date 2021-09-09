d2e2d1ff7162ef733e50e83a61cf3c75b065cccb
msizanoen1 <qtmlabs@protonmail.com>
2021-09-09 20:31:01 +0700

    Improve compatibility with pyinstaller

--- waydroid.py
+++ waydroid.py
@@ -4,6 +4,16 @@
 # PYTHON_ARGCOMPLETE_OK
 import sys
 import tools
+import os
+
+lp_key = 'LD_LIBRARY_PATH'  # for GNU/Linux and *BSD.
+lp_orig = os.environ.get(lp_key + '_ORIG')
+if lp_orig is not None:
+    os.environ[lp_key] = lp_orig  # restore the original, unmodified value
+else:
+    # This happens when LD_LIBRARY_PATH was not set.
+    # Remove the env var as a last resort:
+    os.environ.pop(lp_key, None)
 
 if __name__ == "__main__":
     sys.exit(tools.main())
