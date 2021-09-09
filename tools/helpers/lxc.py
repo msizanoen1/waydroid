d1aed9d84036106c7b3757b2f9eb433fa9765769
msizanoen1 <qtmlabs@protonmail.com>
2021-09-09 20:56:38 +0700

    Add /dev/null and /dev/tty

--- tools/helpers/lxc.py
+++ tools/helpers/lxc.py
@@ -40,6 +40,7 @@ def generate_nodes_lxc_config(args):
     make_entry("/dev/zero")
     make_entry("/dev/null")
     make_entry("/dev/full")
+    make_entry("/dev/tty")
     make_entry("/dev/ashmem", check=False)
     make_entry("/dev/fuse")
     make_entry("/dev/ion")
